a43564c5bb3453db53818375600221239a4cbd5c
Ritesh singh <58039285+ritesh6916@users.noreply.github.com>
2024-06-15 14:46:01 +0530

    default configured code.

---
+++ src/main/java/info/ritesh/scm/TheSmartContactManagerApplication.java
@@ -0,0 +1,13 @@
+package info.ritesh.scm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheSmartContactManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheSmartContactManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/info/ritesh/scm/TheSmartContactManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package info.ritesh.scm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheSmartContactManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
