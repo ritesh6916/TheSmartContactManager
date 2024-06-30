adb4598e12cf9316d9a7f1e3390d7d0c3ea27a0f
Ritesh singh <58039285+ritesh6916@users.noreply.github.com>
2024-06-30 19:30:55 +0530

    created controllers and modified nav-bar.

--- src/main/java/info/ritesh/scm/controllers/PageController.java
+++ src/main/java/info/ritesh/scm/controllers/PageController.java
@@ -2,22 +2,37 @@ package info.ritesh.scm.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PageController {
 
-	@RequestMapping(path = "/home", method = RequestMethod.GET)
+	@GetMapping("/home")
 	public String requestMethodName(Model m) {
 
 		m.addAttribute("name", "Ritesh");
-		m.addAttribute("git","https://github.com/ritesh6916");
+		m.addAttribute("git", "https://github.com/ritesh6916");
 		return "home";
 	}
 
-	@RequestMapping("/about")
+	@GetMapping("/about")
 	public String showAbout() {
 		return "about";
 	}
+
+	@GetMapping("/register")
+	public String getRegisterPage() {
+		return "register";
+	}
+
+	@GetMapping("/login")
+	public String getLoginpPage() {
+		return "login";
+	}
+
+	@GetMapping("/contact")
+	public String getContactpPage() {
+		return "contact";
+	}
+
 }
