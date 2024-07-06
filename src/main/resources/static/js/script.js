a738a7b3c9df40726c7332569a9f3ab9aa31a473
Ritesh singh <58039285+ritesh6916@users.noreply.github.com>
2024-07-06 07:03:15 +0530

    made const ot let

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -13,7 +13,7 @@ function changeTheme() {
 
 	changeThemeButton.addEventListener("click", (event) => {
 
-		const oldTheme = currentTheme;
+		let oldTheme = currentTheme;
 		if (currentTheme === "dark") {
 			currentTheme = "light";
 		} else {
